233ebe068a34a7a05dbbfa34ce8b5f8fb8c0e54c
Jason Keane <jason@keane.id>
2020-04-01 21:55:19 +0100

    Another fix

--- setup.sh
+++ setup.sh
@@ -38,7 +38,7 @@ then
 
     # Chrome
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-    sudo dnf install ./google-chrome-stable_current_x86_64.rpm
+    sudo dnf install -y ./google-chrome-stable_current_x86_64.rpm
 
     if [ "$INSTALL_TYPE" == "full" ]
     then
